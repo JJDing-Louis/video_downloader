0afc3870cca09ddbb8f297c95009de6d26e76cd2
J.J.Ding <louis7012161@gmail.com>
2021-04-07 23:13:40 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Change URL

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -4,13 +4,12 @@
 '''
 
 import requests
-import time
 import m3u8
 import os
 
 #網址參數
-url = "https://www.dgzhuorui.com:65/20201028/6XzCYk32/1200kb/hls/index.m3u8"
-url_header = "https://www.dgzhuorui.com:65"
+url = "https://cdn003.wokuzy.com/s03/d/0/0/ae3f99dd9f6c46c8d6b95a7658f89/hls/h264/index.m3u8"
+url_header = "https://cdn003.wokuzy.com/s03/d/0/0/ae3f99dd9f6c46c8d6b95a7658f89/hls/h264/"
 
 #m3u8解析器
 playlist = m3u8.load(url)
@@ -19,9 +18,10 @@ data = playlist.dumps() #此步驟，會把資料轉成"字串"
 #print(data)
 list = data.split('\n') #以換行字元分隔，轉成串列
 #print(list)
-list = list[3:-2] #去掉頭、尾的資訊
+list = list[4:-2] #去掉頭、尾的資訊
 #print(list)
 print(f"共有{len(list)}個檔案")
+
 #以下是取奇數的資訊
 list_a =[]
 for i in list:
@@ -43,22 +43,21 @@ for i in list_a:
 root = "D://download_video//"
 if not os.path.exists(root):
     os.mkdir(root)
-k=0
+
 for i in link:
-    str= f"str{k}.ts"
-    filename = str
-    path = root + filename
+    #str= f"str{k}.ts"
+    filename = i[-14:-1]
+    print(i)
+    print(filename)
+    path = root
     r =requests.get(i)
     with open(path, 'wb') as f:
         f.write(r.content)
         f.close()
-        print('\r'+filename + "---> OK\n", end='')
-    k +=1
-
-
+        print('\r'+filename + "---> OK", end='')
 
 print("done")
-
+'''
 #以下是合併TS檔，並轉為mp4
 print("開始合併...")
 root = "D://download_video//"
@@ -68,5 +67,6 @@ if not os.path.exists(outdir):
     os.mkdir(outdir)
 os.system("copy /b *.ts new.mp4")
 os.system("move new.mp4 {}".format(outdir))
-print("结束合并...")
 
+print("结束合并...")
+'''
